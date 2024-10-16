656645be1ac92b55c44c75c55cb7f46ab46f764c
abhinayaanair <abhinayapersonal11@gmail.com>
2024-10-16 11:24:28 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='abhinaya nair',
+    author_email='abhinayapersonal11@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
