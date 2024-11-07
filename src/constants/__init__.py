6d9c85a2b6882d27e38b21fda7613530ed72600b
abhinayaanair <abhinayapersonal11@gmail.com>
2024-11-08 04:52:18 +0530

    till generate analysis

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://algorythm:jLnqLVWfxfZPQxQA@cluster0.3seb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
